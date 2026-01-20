1bbc5cb9b1c58da6cd38263b5ac739a6095a7942
britojp <joaopedrodebritotome@discente.ufg.br>
2026-01-20 07:52:15 -0300

    Corrigir roteamento SPA para GitHub Pages com 404.html e base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -13,16 +13,20 @@ import Catalogo from "@/pages/Catalogo";
 import Revendas from "@/pages/Revendas";
 import NotFound from "@/pages/not-found";
 
+const basePath = import.meta.env.PROD ? "/lp-shade-express" : "";
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/empresa" component={Empresa} />
-      <Route path="/produtos" component={Produtos} />
-      <Route path="/catalogo" component={Catalogo} />
-      <Route path="/revendas" component={Revendas} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={basePath}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/empresa" component={Empresa} />
+        <Route path="/produtos" component={Produtos} />
+        <Route path="/catalogo" component={Catalogo} />
+        <Route path="/revendas" component={Revendas} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
